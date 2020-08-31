11a217a20333454d710757b6abdfbfc8ef1dde15
ranaaditya <mail2aditya.rana98@gmail.com>
2020-09-01 03:18:10 +0530

    Adding boilerplate code for app

---
+++ app/src/main/java/com/ranaaditya/opengl_es_through_examples/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ranaaditya.opengl_es_through_examples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
