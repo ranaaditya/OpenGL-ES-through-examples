0a3042b7d9f9a30a5a02b620c6cda1d307167d59
ranaaditya <mail2aditya.rana98@gmail.com>
2020-09-01 04:32:05 +0530

    Add flash feature

--- app/src/main/java/com/ranaaditya/opengl_es_through_examples/opengl/openglRenderer.kt
+++ app/src/main/java/com/ranaaditya/opengl_es_through_examples/opengl/openglRenderer.kt
@@ -4,12 +4,16 @@ import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+import kotlin.math.sin
 
 
 /**
  * @author Aditya Rana
  */
 open class openglRenderer : GLSurfaceView.Renderer {
+
+    private val flash_duration = 1000.0
+    var red = 1.0f
 
     /**
      * empty constructor
@@ -18,7 +22,14 @@ open class openglRenderer : GLSurfaceView.Renderer {
 
     override fun onDrawFrame(gl: GL10?) {
 
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glClearColor(red, 0.0f, 0.0f, 1.0f)
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+        /**
+         * calculating the value for red color depends on time and flash time
+         */
+        red =
+            ((sin(System.currentTimeMillis() * 2 * Math.PI / flash_duration) * 0.5) + 0.5).toFloat()
 
     }
 
@@ -30,8 +41,9 @@ open class openglRenderer : GLSurfaceView.Renderer {
 
         /**
          *  your color  in RGBA format for rendering goes here
+         *  flashing the red and black color at an interval of 1000 milliseconds
          */
-        GLES20.glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+        GLES20.glClearColor(red, 0.0f, 0.0f, 1.0f)
 
     }
 
